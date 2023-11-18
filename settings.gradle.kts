e048bc9075413cb78832383a570ce2d49b424e1d
Zeek1999 <b.b.1@yandex.ru>
2023-11-18 20:31:29 +0300

    messege

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,16 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins{
+        val kotlinVersion: String by settings
+
+        kotlin("jvm") version kotlinVersion
+    }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
+
 
 rootProject.name = "Lesson_kotlin1"
+
+include("m1l1-first-app")
+include("m1l1-first-app")
